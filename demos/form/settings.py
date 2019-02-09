c20b09f7dc5dc3675fde48ffaf0115e5cbd3398b
Frank zheng <zhengger@gmail.com>
2019-02-09 20:48:20 +0800

    form learning

---
+++ demos/form/settings.py
@@ -0,0 +1,8 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
